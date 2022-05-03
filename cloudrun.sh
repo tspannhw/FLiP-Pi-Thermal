05a9cb5b9822308ea1834df36ea84f58e7efd588
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2022-05-03 08:16:09 -0400

    Create cloudrun.sh

---
+++ cloudrun.sh
@@ -0,0 +1,3 @@
+python3 /opt/demo/cloudthermal.py -su pulsar+ssl://d.s.snio.cloud:6651 -t pithermal --auth-params '{"private_key":"/opt/demo/sndemo-tspann.json","issuer_url":"https://auth.streamnative.cloud","audience":"urn:sn:pulsar:s:acluster"}'
+
+# No trailing /!!!!   https://auth.streamnative.cloud
